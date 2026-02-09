bb1c13ad25e3be6ae8c0ea9595c31c04f536e07e
DandgeRohan <dandgerohan00@gmail.com>
2026-02-09 13:21:01 +0530

    fix:import issues

--- src/components/home/FeaturesSection.jsx
+++ src/components/home/FeaturesSection.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import f1 from "../../assets/images/feature/f1.png";
-import f2 from "../../assets/images/feature/f2.png";
-import f3 from "../../assets/images/feature/f3.png";
-import f4 from "../../assets/images/feature/f4.png";
 
 const FeaturesSection = () => {
   return (
     <section>
       <div>
         <div>
-          
+
         </div>
       </div>
     </section>
