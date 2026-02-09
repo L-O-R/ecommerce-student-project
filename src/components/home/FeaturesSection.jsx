1fa1ccffa9490a9f13ba7d3a16256a172eeb3192
DandgeRohan <dandgerohan00@gmail.com>
2026-02-09 12:56:58 +0530

    new: added image folder in public folder

--- src/components/home/FeaturesSection.jsx
+++ src/components/home/FeaturesSection.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
+import f1 from "../../assets/images/feature/f1.png";
+import f2 from "../../assets/images/feature/f2.png";
+import f3 from "../../assets/images/feature/f3.png";
+import f4 from "../../assets/images/feature/f4.png";
 
 const FeaturesSection = () => {
   return (
-    <div>
-      
-    </div>
+    <section>
+      <div>
+        <div>
+          
+        </div>
+      </div>
+    </section>
   )
 }
 
