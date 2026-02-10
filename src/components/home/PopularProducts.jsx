f12a66338da5908e2a160e2263e593844ffe97a8
Vattikundala Akhila <akhila.vattikundala.0511@gmail.com>
2026-02-10 11:48:09 +0530

    created PopularProducts component (#8)

--- src/components/home/PopularProducts.jsx
+++ src/components/home/PopularProducts.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import ProductCard from "../shop/ProductCard";
+import productsData from "../../data/products.json";
 
 const PopularProducts = () => {
+  const popularProducts = productsData.products.slice(0,10);
+  // this will return the first 10 products from all products
+
   return (
-    <div>
-      
-    </div>
+    <section>
+      <div>
+      <h2>Popular Products</h2>
+      <a href="">View all -- </a>
+      </div>
+
+ 
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5'>
+
+     
+
+    {
+      popularProducts.map((product)=>{
+        return(
+          <ProductCard key={product.id} product={product}/>
+
+        )
+      })
+    }
+     </div>
+    </section>
   )
 }
 
