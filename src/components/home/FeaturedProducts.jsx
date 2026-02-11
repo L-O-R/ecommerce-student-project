314d2b86ce2d7aa6a5b7eaad523e0174ef140d67
Vattikundala Akhila <akhila.vattikundala.0511@gmail.com>
2026-02-11 13:48:56 +0530

    Akhila/product sections (#12)
    * created PopularProducts component
    
    * feat:- New Feature

--- src/components/home/FeaturedProducts.jsx
+++ src/components/home/FeaturedProducts.jsx
@@ -9,20 +9,24 @@ const FeaturedProducts = () => {
   return (
     <div>
       <section className='py-10'>
-      <div className="layout flex justify-between items-center">
+      <div className="layout flex justify-between items-center ">
       <h2 className="font-medium text-xl">Featured Products</h2>
       <a href="" className='text-sm text-primary'>View all -- </a>
       </div>
 
  
-      <div className=' layout py-3 flex '>
+       <div className='layout py-3 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 '>
+
 
      
 
     {
       popularProducts.map((product)=>{
         return(
-          <ProductCard key={product.id} product={product}/>
+          <div key={product.id} className='flex-1  min-w-[160px] md:min-w-[170px]'> 
+
+          <ProductCard  product={product}/>
+          </div>
 
         )
       })
