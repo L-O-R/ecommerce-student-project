a8f68bf1f5ab058053c4ff7a08c5e1088a1e6760
kasarshravani04 <shravanikasar5@gmail.com>
2026-02-23 14:43:45 +0530

    feat:SignIn Page, TeamSection & Fixing MainHeader

--- src/components/about/TeamSection.jsx
+++ src/components/about/TeamSection.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+import arrowLeft from "../../assets/icons/arrow.png";
+import arrowRight from "../../assets/icons/arrow.png";
 import member1 from "../../assets/images/Teammember1.png";
 import member2 from "../../assets/images/Teammember2.png";
 import member3 from "../../assets/images/Teammember3.png";
 import member4 from "../../assets/images/Teammember4.png";
+import facebook from '../../assets/icons/facebookimg.png';
+import twitter from "../../assets/icons/twitterimg.png";
+import pinterest from "../../assets/icons/pinterestimg.png";
+import instragram from "../../assets/icons/instragramimg.png";
+import { Link } from "react-router-dom";
 
 const teamImages_navlinks = [
     {
@@ -9,7 +17,10 @@ const teamImages_navlinks = [
        image:member1,
        name:"Jenny Wilson",
        position:"Ceo & Founder",
-
+       faceBook:"link #",
+       twitter:"link #",
+       pinterest:"link #",
+       instragram:"link #",
     },
     {
         id:"2",
@@ -29,44 +40,140 @@ const teamImages_navlinks = [
         name:"Robert Fox",
         position:"Senior Farmer Manager",
     },
+    {
+        id:"5",
+        image:member1,
+        name:"John Doe",
+        position:"Junior Manager",
+    },
+    {
+        id:"6",
+        image:member2,
+        name:"Albert",
+        position:"coworker",
+    },
 ]
+const TeamSection= () => {
+  const teammember = teamImages_navlinks;
+  const[index, setIndex] = useState(0); 
+ 
+  let itemsPerView = 4;
+ if(window.innerWidth <= 1040 && window.innerWidth >= 540){
+    itemsPerView = 3
+  }else if(window.innerWidth <= 540){
+    itemsPerView = 1
+  }
 
-const TeamSection = () => {
-    return (
-        <div className="bg-linear-to-180 from-gray-05 to-white my-20">
-            <div className="px-16 flex flex-col text-center justify-center">
+
+ const handleNext = () => {
+    console.log(index < teammember.length - itemsPerView)
+  if (index < teammember.length - itemsPerView) {
+    setIndex(prev => prev + 1);
+
+  }
+};
+
+const handlePrev = () => {
+  if (index > 0) {
+    setIndex(prev => prev - 1);
+  }
+};
+return (
+    <section className="bg-linear-to-180 from-gray-05 to-white my-20 layout relative">
+        <header className=" flex flex-col text-center justify-center">
                 <h2 className="text-gray-9 text-3xl font-semibold mt-8">Our Awesome Team</h2>
-                <p className="text-md text-gray-6 gap-12">
+                <p className="text-md text-gray-6 max-w-lg mx-auto">
                    Pellentesque a ante vulputate leo porttitor luctus sed eget eros. 
                    Nulla et rhoncus neque. Duis non diam eget est luctus tincidunt a a mi.
                 </p>  
-            </div>
-            <div className=" mt-16 flex justify-around gap-4">
-                  {teamImages_navlinks.map((item) => (
-              <div
-                  key={item.id}
-                  className="rounded-lg overflow-hidden shadow hover:shadow-lg transition duration-300"
+        </header>
+       
+        <div className="relative flex overflow-x-hidden mt-10 transition-all duration-500 ease-linear">
+            
+
+
+            {/* cards */}
+             {teamImages_navlinks.slice(index, index + itemsPerView).map((item) => (
+                <div className="shrink-0 w-full sm:w-1/3 lg:w-1/4 sm:px-4">
+                <div
+                    key={item.id}
+                   className=" group rounded-lg overflow-hidden shadow hover:shadow-lg transition duration-300"
                 >
-                 {/* Image */}
+                  <div className="relative">
                 <img
-                src={item.image}
-                alt={item.name}
-                className="w-full h-60 object-cover"
+                      src={item.image}
+                      alt={item.name}
+                      className="w-full h-60 object-cover"
                 />
-   
-               {/* Text Section */}
+                <div className="bg-black/40 backdrop-blur-[1px] absolute -top-100  group-hover:top-0 left-0 w-full h-full flex items-center justify-center gap-3 text-white">
+                  <Link to= {item?.faceBook}>
+                  <div className="w-10 h-10 flex items-center justify-center rounded-full hover:bg-primary transition duration-200 cursor-pointer">
+                  <img src={facebook} alt="facebook"/>
+                  </div>
+                  </Link>
+                   <Link to= {item?.twitter}>
+                   <div className="w-10 h-10 flex items-center justify-center rounded-full hover:bg-primary transition duration-200 cursor-pointer">
+                  <img src={twitter} alt="twitter"/>
+                  </div> 
+                  </Link>
+                   <Link to= {item?.pinterest}>
+                   <div className="w-10 h-10 flex items-center justify-center rounded-full hover:bg-primary transition duration-200 cursor-pointer">
+                  <img src={pinterest} alt="pinterest"/>
+                  </div>
+                  </Link>
+                   <Link to= {item?.instragram}>
+                   <div className="w-10 h-10 flex items-center justify-center rounded-full hover:bg-primary transition duration-200 cursor-pointer">
+                  <img src={instragram} alt="instragram"/>
+                  </div>
+                  </Link>
+                   
+                </div>
+                </div>
                 <div className="pl-5 my-5 gap-5">
                   <h3 className="text-md font-medium ">{item.name}</h3>
                   <p className="text-gray-500 text-sm">{item.position}</p>
                </div>
-             </div>
-             ))}
-            </div>
+               </div>
+              </div>
+               
+               ))}
+        </div>
+
+
+        
+                    {/* perivious btn */}
+            <button
+                onClick={handlePrev}
+                disabled={index === 0}
+                className="absolute left-8 top-[60%] w-[45px] h-[45px] flex items-center justify-center border rounded-full outline outline-gray-100 hover:bg-gray-4 disabled:hidden cursor-pointer"
+                >
+                <img
+                 src={arrowRight}
+                 className=" w-4"
+                 alt="next"
+                 />
+               </button>
+
+
+                    {/* next btn */}
+            <button
+                onClick={handleNext}
+                disabled={index >= teammember.length - itemsPerView}
+                className="absolute right-12 top-[60%] w-[45px] h-[45px] flex items-center justify-center border rounded-full outline outline-gray-100 hover:bg-gray-4 disabled:hidden cursor-pointer"
+                >
+                <img
+                 src={arrowRight}
+                 className="rotate-180 w-4"
+                 alt="next"
+                 />
+               </button>
+
 
 
             
-        </div>
+    </section>
     )
-}
+};
+
 
 export default TeamSection;
--- src/pages/auth/SignIn.jsx
+++ src/pages/auth/SignIn.jsx
@@ -1,45 +1,58 @@
 import { useState } from "react";
 import {NavLink,Link} from "react-router-dom";
 import Button from "../../components/common/Button.jsx";
+import passwordicon from "../../assets/icons/passwordicon.png";
 
 const Signin = () => {
     const[email,setEmail] = useState("");
     const [password,setPassword] = useState("");
+    const [showPassword,setShowPassword] = useState(false);
     const [remember,setRemember] = useState(false);
+    const [error,setError] = useState("");
+    const [success,setSuccess] = useState("");
+
     
-const userData = {
-    email,
-    password
-};
-localStorage.setItem("registeredUser" , JSON.stringify(userData));
 
 const handleSubmit = (e) => {
     e.preventDefault();
+
+    setError("");
+    setSuccess("");
    
-    const storedUser = JSON.parse(localStorage.getItem("regsiteredUser"));
+    const storedUser = JSON.parse(localStorage.getItem("user")) || [];
     if(!storedUser) {
-        alert("User Not Found,Please Register");
+        setError("User Not Found,Please Register First");
         return;
     }
     
-    if (email === storedUser.email && password === storedUser.password){
-        alert("Login Successful")
+    if (storedUser.email!== email){
+        console.log(storedUser.email)
+        console.log(email)
+        setError("Email not found");
+        return;
     }
-     const loginData = {email};
+    if (storedUser.password!== password){
+        setError("Incorrect Password");
+        return;
+    }
+
+    setSuccess("Login Successful");
+     
+    
+    const loginData = {email};
 
    if (remember) {
-    localStorage.setItem("user", JSON.stringify(loginData));
+    localStorage.setItem("loggedInUser", JSON.stringify(loginData));
       } 
     else {
-    sessionStorage.setItem("user", JSON.stringify(loginData));
+    sessionStorage.setItem("loggedInUser", JSON.stringify(loginData));
     }
-    console.log("Invalid Email & Password");
 
 };
 
     return (
-        <div className=" flex items-center justify-center bg-white px-4 py-16">
-            <div className="rounded-2xl shadow-[0px_0px_56px_#00260314] p-10">
+        <div className="flex items-center justify-center bg-white px-4 py-16">
+            <div className="w-full max-w-xl rounded-2xl shadow-[0px_0px_56px_#00260314] p-10">
 
             <h1 className="text-center mb-8 text-4xl font-semibold text-gray-9" >
             Sign In
@@ -57,18 +70,30 @@ const handleSubmit = (e) => {
                    required
                    className="w-full p-4 outline outline-gray-1 rounded-xl hover:outline-gray-9 transition duration-200"
                    />
+                  <div className="relative w-full">
                    <label  htmlFor="email" className="hidden">password</label>
                    <input
-                     type = "password" 
+                     type = {
+                        showPassword ? "text" : "password"
+                     } 
+                     
                      placeholder="Enter Your Password"
                      value={password}
                      id="password"
                      name="password"
                      onChange={(e) => setPassword(e.target.value)}
                      required
-                     className="w-full p-4 outline outline-gray-1 rounded-xl hover:outline-gray-9 transition duration-200"
+                     className="w-full p-4 pr-12 outline outline-gray-1 rounded-xl hover:outline-gray-9 transition duration-200"
                    />
-                    <div className="flex items-center justify-between mt-4">
+                    <img
+                    src={passwordicon}
+                    alt="toggle password"
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="absolute right-4 top-1/2 -translate-y-1/2 w-5 h-4 cursor-pointer"
+                    />
+                </div>
+
+                 <div className="flex items-center justify-between mt-4">
                         <label htmlFor="remember" className="flex items-center gap-2 cursor-pointer">
                         <input
                         type="checkbox"
@@ -85,6 +110,17 @@ const handleSubmit = (e) => {
                            Forgot Password?  
                         </Link> 
                     </div>
+                     {error && (
+                           <p className="text-red-500 text-sm text-center">
+                     {error}
+                           </p>
+                    )}
+
+                     {success && (
+                          <p className="text-green-600 text-sm text-center">
+                     {success}
+                          </p>
+                    )}
 
                    <Button
                     type="submit"
